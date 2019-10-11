09b905997479db86a45008d847d5e8fb566b6246
Gina Sekhar <ginasekhar@yahoo.com>
2019-10-11 18:05:56 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -25,9 +25,9 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  if (phone.match(/\(?\d{3}\)?[\da-zA-Z]{3}[\s-]?[\da-zA-Z]{4}/) 
-    || phone.match(/[0-9]{10}/) 
-    || phone.match(/\d{3}\s\d{3}\s\d{4}/)  )
+  if phone.match(/\(?\d{3}\)?[\da-zA-Z]{3}[\s-]?[\da-zA-Z]{4}/) 
+    #|| phone.match(/[0-9]{10}/) 
+    || phone.match(/\d{3}\s\d{3}\s\d{4}/)  
       return TRUE
   else
     return FALSE
