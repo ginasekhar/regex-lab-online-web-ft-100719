900201fd08098391b9769de91d3d835c7e22bc04
Gina Sekhar <ginasekhar@yahoo.com>
2019-10-11 17:52:30 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -28,7 +28,8 @@ def valid_phone_number?(phone)
 valid_numbers = ["2438894546", "(718)891-1313", "234 435 9978", "(800)4261134"]
 invalid_numbers = ["28894546", "(718)891-13135", "234 43 9978", "(800)IloveNY"]
 
-  if (phone.match(/[0-9]{10}/) || phone.match(/\d{3}\s\d{3}\s\d{4}/)  ||
+  if (phone.match(/\(? \d{3} \)? [\d a-z A-Z]{3}[\s-]?[\d a-z A-Z]{4}/)
+    #(phone.match(/[0-9]{10}/) || phone.match(/\d{3}\s\d{3}\s\d{4}/)  ||
     #|| phone.match(/(\d{3}\)[\da-ZA-Z]{7}/) 
     #||  phone.match(/\(* \d{3} \)* [\da-zA-Z]{3}[\s-]*[\da-zA-Z]{4}\)
     return TRUE
