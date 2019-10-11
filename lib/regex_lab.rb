eccc1bdd72d7dfdf807f87acd3a10d1419e0390e
Gina Sekhar <ginasekhar@yahoo.com>
2019-10-11 17:30:17 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -28,8 +28,9 @@ def valid_phone_number?(phone)
 valid_numbers = ["2438894546", "(718)891-1313", "234 435 9978", "(800)4261134"]
 invalid_numbers = ["28894546", "(718)891-13135", "234 43 9978", "(800)IloveNY"]
 
-  if phone.match(/[0-9]{10}/) || phone.match(/\d{3}\s\d{3}\s\d{4}/) 
-    || phone.match(/\(\d{3}\)[\da-ZA-Z]{7}/) 
+  if 
+    #phone.match(/[0-9]{10}/) || phone.match(/\d{3}\s\d{3}\s\d{4}/) 
+    #|| phone.match(/\(\d{3}\)[\da-ZA-Z]{7}/) 
     #||  phone.match(/\(\d{3}\)[\da-zA-Z]{3}][\s-][\da-zA-Z]{4}\)
     return TRUE
   else
